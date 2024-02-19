cba6c3c4bd01b1bbe7c62724097262a289c6e640
Laeti013 <derohdenlaetitia7@gmail.com>
2024-02-19 15:09:07 +0100

    add map html, css, js

---
+++ map.js
@@ -0,0 +1,38 @@
+var map = L.map("map").setView([43.3, 5.4], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+var marker = L.marker([43.3, 5.4]).addTo(map);
+
+var circle = L.circle([43.3, 5.4], {
+  color: "red",
+  fillColor: "#f03",
+  fillOpacity: 0.5,
+  radius: 500,
+}).addTo(map);
+
+var polygon = L.polygon([
+  [43.3, 5.4],
+  [43.3, 5.4],
+  [43.3, 5.4],
+]).addTo(map);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+var popup = L.popup()
+  .setLatLng([43.3, 5.4])
+  .setContent("I am a standalone popup.")
+  .openOn(map);
+
+function onMapClick(e) {
+  alert("You clicked the map at " + e.latlng);
+}
+
+map.on("click", onMapClick);
+
