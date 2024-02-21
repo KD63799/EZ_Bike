4533c6a9fe4e4835cddddb2812f6eb4b8b4a3244
Laeti013 <derohdenlaetitia7@gmail.com>
2024-02-21 10:54:44 +0100

    add coming soon

---
+++ assets/js/mode_d_emploi.js
@@ -0,0 +1,30 @@
+const carouselItems = document.querySelectorAll(".card");
+let currentIndex = 0;
+
+function showSlide(index) {
+  // Cacher toutes les cartes du carousel
+  carouselItems.forEach((item) => {
+    item.style.display = "none";
+  });
+
+  // Afficher la carte à l'index spécifié
+  carouselItems[index].style.display = "block";
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % carouselItems.length;
+  showSlide(currentIndex);
+}
+
+function previousSlide() {
+  currentIndex =
+    (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+  showSlide(currentIndex);
+}
+
+// Afficher la première carte
+showSlide(currentIndex);
+
+// Met en place les écouteurs d'événements pour les boutons précédent et suivant
+document.getElementById("nextBtn").addEventListener("click", nextSlide);
+document.getElementById("prevBtn").addEventListener("click", previousSlide);
